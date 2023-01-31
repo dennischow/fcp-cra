ff98f320d8fb663b87fde0b0c22799adb0c3c880
Dennis Chow <dennischow@Denniss-MBP>
2023-02-01 01:05:58 +0800

    projects-hilite skeleton sttyle

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -4,6 +4,8 @@ import { Helmet } from "react-helmet-async";
 import AppHeroBanner from "../../components/app-hero-banner/app-hero-banner.component";
 import heroBackgroundImg from "../../resources/images/home/polygon-colored-crossed-dark-extend-bg.jpg";
 
+import "./home.styles.scss";
+
 const Home = () => {
     return (
         <Fragment>
@@ -18,8 +20,32 @@ const Home = () => {
                     subHeading="Who cares about things that users see and interact with"
                 />
 
-                <div className="container">
-                    <h2>Home</h2>
+                <div className="section projects-hilite">
+                    <div className="projects-hilite__container">
+                        <div className="projects-hilite__teaser">
+                            <h2 className="projects-hilite__teaser-title">Creativity</h2>
+                            <p className="projects-hilite__teaser-des">There are different ways to be creative. Find out what mine are.</p>
+                            <a className="projects-hilite__teaser-link" href="#">Check it out</a>
+                        </div>
+                        <div className="projects-hilite__entries">
+                            <div className="projects-hilite__entry">
+                                <p className="projects-hilite__entry-cat">Web Development / Design</p>
+                                <a className="projects-hilite__entry-link" href="#">Frank.co.th - Online Insurance Platform</a>
+                            </div>
+                            <div className="projects-hilite__entry">
+                                <p className="projects-hilite__entry-cat">Web Development / Design</p>
+                                <a className="projects-hilite__entry-link" href="#">Frank.co.th - Online Insurance Platform</a>
+                            </div>
+                            <div className="projects-hilite__entry">
+                                <p className="projects-hilite__entry-cat">Web Development / Design</p>
+                                <a className="projects-hilite__entry-link" href="#">Frank.co.th - Online Insurance Platform</a>
+                            </div>
+                            <div className="projects-hilite__entry">
+                                <p className="projects-hilite__entry-cat">Web Development / Design</p>
+                                <a className="projects-hilite__entry-link" href="#">Frank.co.th - Online Insurance Platform</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
