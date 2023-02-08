7a01cf7b1e01647f93f9faafdb3d90c82e5b6e3c
Dennis Chow <dennischow@Denniss-MBP>
2023-02-08 18:00:52 +0800

    Added icons in app-nav

--- src/components/app-nav/app-nav.component.jsx
+++ src/components/app-nav/app-nav.component.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { FaHome, FaUser, FaRegLightbulb, FaRss, FaRegEnvelope, FaSearch } from "react-icons/fa";
 
 import * as CONSTANTS from "../../common/constants";
 import "./app-nav.styles.scss";
@@ -9,12 +10,22 @@ const AppNav = ({ props, ...otherProps }) => {
             <ul className="app-nav__list">
                 <li className="app-nav__item">
                     <NavLink className="app-nav__link" to={CONSTANTS.ROUTES.home.path}>
-                        {CONSTANTS.ROUTES.home.name}
+                        <span className="app-nav__icon">
+                            <FaHome />
+                        </span>
+                        <span className="app-nav__text">
+                            {CONSTANTS.ROUTES.home.name}
+                        </span>
                     </NavLink>
                 </li>
                 <li className="app-nav__item">
                     <NavLink className="app-nav__link" to={CONSTANTS.ROUTES.about.path}>
-                        {CONSTANTS.ROUTES.about.name}
+                        <span className="app-nav__icon">
+                            <FaUser />
+                        </span>
+                        <span className="app-nav__text">
+                            {CONSTANTS.ROUTES.about.name}
+                        </span>
                     </NavLink>
                 </li>
             </ul>
